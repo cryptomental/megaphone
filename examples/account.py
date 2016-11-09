7629cbc012e0bc0f515857be674c61c33852aa2d
Cryptomental <cryptomental.com@gmail.com>
2016-11-09 12:02:55 +0100

    examples: Account.py example cleanup.
    - PEP compliance
    - removed unused reports
    - refactored not to call Account.filter_by_date() twice.

--- examples/account.py
+++ examples/account.py
@@ -1,21 +1,21 @@
-from pprint import pprint
-
 from megaphone.base import Account
 from megaphone.helpers import parse_payout, is_comment
 
-# get all posts from October
+# get all posts and comments from October
 account = Account("kiwi")
 START_DATE = "2016-10-01T00:00:00"
 END_DATE = "2016-11-01T00:00:00"
 
-# get posts and comments from August
-Account.filter_by_date(account.history(filter_by="comment"), START_DATE, END_DATE)
+posts = Account.filter_by_date(account.history(filter_by="comment"),
+                               START_DATE, END_DATE)
 
-# get just the titles of only posts from August
-print([x['op']['title'] for x in Account.filter_by_date(account.history(filter_by="comment"), START_DATE, END_DATE) if
-       not is_comment(x['op'])])
+# print titles of top level posts from October
+titles = [x["op"]["title"] for x in posts if not is_comment(x["op"])]
+print(titles)
 
+# get transfers to "null" account
 for event in Account("kiwi").history(filter_by=["transfer"]):
-    transfer = event['op']
-    if transfer['to'] == "null":
-        print("$%.1f :: %s" % (parse_payout(transfer['amount']), transfer['memo']))
+    transfer = event["op"]
+    if transfer["to"] == "null":
+        print("$%.1f :: %s" % (parse_payout(transfer["amount"]),
+                               transfer["memo"]))
