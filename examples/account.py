2e0b672a2e08fd6ea5547ba41e63b18eb07bc165
Cryptomental <dev@cryptomental.com>
2016-11-15 17:32:59 +0100

    examples: Update account example after refactoring.
    Account example can now accept a custom account name as an argument
    and uses refactored methods from megaphone module.

--- examples/account.py
+++ examples/account.py
@@ -1,23 +1,23 @@
 import sys
 
-from megaphone.base import Account
-from megaphone.helpers import parse_payout, is_comment
+from megaphone.account import Account
+from megaphone.helpers import parse_payout, is_comment, filter_by_date
 
 
-# get all posts and comments from October
-if sys.argv[1]:
+if len(sys.argv) == 2:
     account_name = sys.argv[1]
 else:
-    account_name = "kiwi"
+    account_name = "cryptomental"
 
 account = Account(account_name)
+
+# get all posts and comments from October
 START_DATE = "2016-10-01T00:00:00"
 END_DATE = "2016-11-01T00:00:00"
 
-posts = Account.filter_by_date(account.history(filter_by="comment"),
-                               START_DATE, END_DATE)
+posts = filter_by_date(account.history(filter_by="comment"), START_DATE, END_DATE)
 
-# print titles of top level posts from October
+# print titles of top level posts from October 2016
 titles = [x["op"]["title"] for x in posts if not is_comment(x["op"])]
 print(titles)
 
