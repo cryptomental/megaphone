1534fe0c6d5629aac96e3799e8530c11b0b06f04
Cryptomental <dev@cryptomental.com>
2016-11-15 17:32:55 +0100

    examples: Update account methods for multiple blockchains.
    The account methods example executes on STEEM and GOLOS blockchains now.

--- examples/account_methods.py
+++ examples/account_methods.py
@@ -1,22 +1,25 @@
 import sys
 from megaphone.account import Account
+from megaphone.node import Node
 
 if len(sys.argv) == 2:
     account_name = sys.argv[1]
 else:
     account_name = "cryptomental"
 
-account = Account(account_name)
 
-print("Account's power: %s" % account.power)
+accounts = [Account(account_name, Node("steem").default()),
+            Account(account_name, Node("golos").default())]
 
-print("Account's reputation: %s" % account.reputation)
+for account in accounts:
+    print("-" * 80)
+    print("Blockchain: %s" % account.blockchain_name)
+    print("Account's power: %s" % account.power)
+    print("Account's reputation: %s" % account.reputation)
+    print("Three recent posts:")
+    blog = account.get_blog()
+    for post in blog[:3]:
+        print(post['title'])
+    print("Conversion requests: %s" % account.get_conversion_requests())
+    print("Withdraw routes: %s" % account.get_withdraw_routes())
 
-print("Three recent posts:")
-blog = account.get_blog()
-for post in blog[:3]:
-    print(post['title'])
-
-print("Conversion requests: %s" % account.get_conversion_requests())
-
-print("Withdraw routes: %s" % account.get_withdraw_routes())
