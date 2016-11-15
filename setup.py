f2b62169d11546b3db8a3265c79ec1f83e5fd96b
Cryptomental <dev@cryptomental.com>
2016-11-15 17:33:39 +0100

    setup.py: Updated for multiple blockchains.
    Updated tags and general information about the tool.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ VERSION = '1.3.3'
 setup(
     name='megaphone',
     version=VERSION,
-    description='Python Utilities for parsing GOLOS blockchain, forked from SteemTools by @furion',
+    description='Python Utilities for graphene based social blockchains, forked from SteemTools by @furion',
     long_description=open('README.md').read(),
     url='https://github.com/cryptomental/megaphone',
     author='@furion',
@@ -26,7 +26,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
     ],
-    keywords='golos',
+    keywords=['steem', 'golos'],
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
